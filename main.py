80405d524669f4325e74d51aff0739b240fffd98
Prathik Jain <prathik.jain.gn@gmail.com>
2024-09-22 16:46:43 +0530

    Predicition Added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+import dagshub
+dagshub.init(repo_owner='pjluvi', repo_name='Chest-Disease-Classification', mlflow=True)
 
 
 STAGE_NAME = "Data Ingestion stage"
